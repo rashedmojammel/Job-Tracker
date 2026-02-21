7ebd0540275b5a67e18eb0a7428bbd6048c36526
rashedmojammel <rashedmojamme56@gmail.com>
2026-02-22 01:26:50 +0600

    fixed count on interview and rejected

--- script/Interview.js
+++ script/Interview.js
@@ -3,6 +3,27 @@ const buttonElement = document.querySelectorAll(".Interview-btn");
 buttonElement.forEach(btn => {
     btn.addEventListener("click", function () {
         console.log("Button added");
+
+    const badgeElement = document.querySelectorAll(".status-badge");
+    badgeElement.innerText = "Applied";
+
+    //Total Jobs count 
+     const JobsELement = document.getElementById("Jobs-count");
+    const Jobcount = Number(JobsELement.innerText); 
+    console.log(Jobcount);
+
+    //Interview count
+    const InterviewELement = document.getElementById("Interview-count");
+    const count = Number(InterviewELement.innerText); 
+     const countnew = count + 1;
+
+     if(countnew >8){
+        alert("Not available jobs");
+        return;
+    }
+    InterviewELement.innerText = countnew;
+    console.log(countnew);
+
     });
 });
 
@@ -11,5 +32,26 @@ const buttonElement2 = document.querySelectorAll(".Rejected-btn");
 buttonElement2.forEach(btn => {
     btn.addEventListener("click", function () {
         console.log("Button added 2");
+
+    //Total Jobs count 
+     const JobsELement = document.getElementById("Jobs-count");
+    const Jobcount = Number(JobsELement.innerText); 
+    console.log(Jobcount);
+        
+    //Rejected Count 
+    const RejectedELement = document.getElementById("Rejected-count");
+    const Rejectedcount = Number(RejectedELement.innerText); 
+    
+
+    const Rejectedcountnew = Rejectedcount + 1;
+    
+   
+    if( Rejectedcountnew >8){
+        alert("Not available jobs");
+        return;
+    }
+    RejectedELement.innerText = Rejectedcountnew;
     });
+
+    
 });
