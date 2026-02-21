08599e138494031f314b2fffde2764c32e6ffdf6
rashedmojammel <rashedmojamme56@gmail.com>
2026-02-22 00:30:02 +0600

    added interview and rejected  button functionalites

--- script/Interview.js
+++ script/Interview.js
@@ -1,5 +1,15 @@
-document.getElementById("Interview").addEventListener("click",function(){
+const buttonElement = document.querySelectorAll(".Interview-btn");
 
-    console.log("added");
-})
+buttonElement.forEach(btn => {
+    btn.addEventListener("click", function () {
+        console.log("Button added");
+    });
+});
 
+const buttonElement2 = document.querySelectorAll(".Rejected-btn");
+
+buttonElement2.forEach(btn => {
+    btn.addEventListener("click", function () {
+        console.log("Button added 2");
+    });
+});
